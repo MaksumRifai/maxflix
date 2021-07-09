5b4648fb79abf916ec0a5915b81988d348be5e30
Maksum Rifai <maksum.rifai@gmail.com>
2021-07-10 02:33:50 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const Movies = type => {
   let movieAPI = '12bd7c58'
   return axios.create({
-    baseURL: `http://www.omdbapi.com/?apikey=${movieAPI}&${type}`
+    baseURL: `https://www.omdbapi.com/?apikey=${movieAPI}&${type}`
   });
 };
